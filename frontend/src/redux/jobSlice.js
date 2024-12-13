895bca0e6f722393c0ba4bba71bad44e849c13ac
Pray4s <96592781+Pray4s@users.noreply.github.com>
2024-12-13 14:59:19 +0530

    Initial Files

---
+++ frontend/src/redux/jobSlice.js
@@ -0,0 +1,29 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const jobSlice = createSlice({
+    name:"job",
+    initialState:{
+        allJobs: [],
+        allAdminJobs:[],
+        singleJob: null,
+        searchJobByText: "",
+    },
+    reducers:{
+        //actions
+        setAllJobs:(state,action) => {
+            state.allJobs = action.payload;
+        },
+        setSingleJob:(state,action) => {
+            state.singleJob = action.payload;
+        },
+        setAllAdminJobs:(state,action) => {
+            state.allAdminJobs = action.payload;
+        },
+        setsearchJobByText:(state,action) => {
+            state.searchJobByText = action.payload;
+        }
+    }
+});
+
+export const {setAllJobs, setSingleJob, setAllAdminJobs, setsearchJobByText} = jobSlice.actions;   
+export default jobSlice.reducer;
